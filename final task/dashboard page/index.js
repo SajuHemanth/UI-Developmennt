c76e0cff2f10b103880f93af202c3ceed4124418
SajuhemanthB <112956019+SajuhemanthB@users.noreply.github.com>
2022-09-29 12:36:41 +0530

    Add files via upload

---
+++ final task/dashboard page/index.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+  $('#example').DataTable({
+      scrollY: 150,
+      scrollX: true,
+  });
+});
+
+
+// // $('.calendar-wrapper').calendar();function selectDate(date) {
+// //   $('#calendar-wrapper').updateCalendarOptions({
+// //     date: date
+// //   });
+// //   console.log(calendar.getSelectedDate());
+// // }
+
+// // var defaultConfig = {
+// //   weekDayLength: 1,
+// //   date: '08/05/2021',
+// //   onClickDate: selectDate,
+// //   showYearDropdown: true,
+// //   startOnMonday: false,
+// // };
+
+// // var calendar = $('#calendar-wrapper').calendar(defaultConfig);
+// // console.log(calendar.getSelectedDate());
+
+// function selectDate(date) {
+//   $('#calendar-wrapper').updateCalendarOptions({
+//     date: date
+//   });
+//   console.log(calendar.getSelectedDate());
+// }
+
+// var defaultConfig = {
+//   weekDayLength: 1,
+//   date: '08/05/2021',
+//   onClickDate: selectDate,
+//   showYearDropdown: true,
+//   startOnMonday: false,
+// };
+
+// var calendar = $('#calendar-wrapper').calendar(defaultConfig);
+// console.log(calendar.getSelectedDate());
